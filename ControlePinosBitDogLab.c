54d33b12239a472e887434faa7cda2f7859db69b
João Victor de Souza Silva <joao1452019@gmail.com>
2025-01-18 19:27:24 -0300

    Prototipagem do código

--- ControlePinosBitDogLab.c
+++ ControlePinosBitDogLab.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+void AtivarLedVerde();
+void AtivarLedAzul();
+void AtivarLedVermelho();
+void AtivarLedBranco();
+void DesligarLeds();
+void AtivarBuzzer();
+void ReceberComando();
 
+char comando[10];  // Buffer para armazenar comandos
 
-int main()
-{
+int main() {
     stdio_init_all();
-
+    InicializarPinos();
+    
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        ReceberComando();
     }
 }
+
+

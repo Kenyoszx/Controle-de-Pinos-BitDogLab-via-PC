582ceb7b69f2e00a8bd4a427d0e43497ef36970c
João Victor de Souza Silva <joao1452019@gmail.com>
2025-01-18 19:31:53 -0300

    Definir Numeração dos pinos e Tratamento do comando

--- ControlePinosBitDogLab.c
+++ ControlePinosBitDogLab.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_VERDE 11
+#define LED_AZUL 12
+#define LED_VERMELHO 13
+#define BUZZER 21
+
 void AtivarLedVerde();
 void AtivarLedAzul();
 void AtivarLedVermelho();
@@ -19,5 +24,22 @@ int main() {
         ReceberComando();
     }
 }
-
+void ReceberComando(){
+    // Receber o comando 
+    if (scanf("%s", comando) > 0) {
+        if (strcmp(comando, "verde") == 0) {
+            AtivarLedVerde();
+        } else if (strcmp(comando, "azul") == 0) {
+            AtivarLedAzul();
+        } else if (strcmp(comando, "vermelho") == 0) {
+            AtivarLedVermelho();
+        } else if (strcmp(comando, "branco") == 0) {
+            AtivarLedBranco();
+        } else if (strcmp(comando, "desligar") == 0) {
+            DesligarLeds();
+        } else if (strcmp(comando, "buzzer") == 0) {
+            AtivarBuzzer();
+        }
+    }
+}
 
